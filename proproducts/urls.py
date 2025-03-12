6ddc7784bda372c29f2f7b8e395a42bc7f092a9e
herbew <hebrew@gmail.com>
2025-03-12 19:09:40 +0700

    hide admin url

--- proproducts/urls.py
+++ proproducts/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('product/', include('products.urls',namespace='products'))
 ]
